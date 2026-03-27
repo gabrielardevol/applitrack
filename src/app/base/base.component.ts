2670ce0e4c2246c8f24eb6757634f4c009d44a9c
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 12:47:31 +0100

    DEV-0002 feat/ added RouterOutlet to base component

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-base',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss',
 })
