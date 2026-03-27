9ef93ae681c7d8d1874f9921c3f892477dc0a9b8
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 13:04:31 +0100

    DEV-0003 feat/ added navigation

--- src/app/base-page/base-page.component.ts
+++ src/app/base-page/base-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-base-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './base-page.component.html',
   styleUrl: './base-page.component.scss',
 })
