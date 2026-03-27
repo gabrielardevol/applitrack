e421d5376a7acc1a8aa128b11c4920ef40da6506
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 13:16:06 +0100

    DEV-0001 refactor/ moved pages to /pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { BasePageComponent } from './base-page/base-page.component';
-import { ContactsPage } from './contacts-page/contacts-page';
-import { DashboardsPage } from './dashboards-page/dashboards-page';
-import { OffersPage } from './offers-page/offers-page';
-import { TipsPage } from './tips-page/tips-page';
-import { RoutinePage } from './routine-page/routine-page';
+import { ContactsPage } from './pages/contacts-page/contacts-page';
+import { DashboardsPage } from './pages/dashboards-page/dashboards-page';
+import { OffersPage } from './pages/offers-page/offers-page';
+import { TipsPage } from './pages/tips-page/tips-page';
+import { RoutinePage } from './pages/routine-page/routine-page';
 
 export const routes: Routes = [
     {
