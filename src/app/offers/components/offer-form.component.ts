f605e8f7db8bf35fb8381bcc6d9396f813a43fe4
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 20:03:55 +0100

    DEV-0006 refactor/ offer form

--- src/app/offers/components/offer-form.component.ts
+++ src/app/offers/components/offer-form.component.ts
@@ -2,6 +2,23 @@ import { Component, inject, signal } from '@angular/core';
 import { OffersService } from '../../services/offers-service';
 import { Offer, OFFER_MODALITIES, OFFER_TYPES, OfferForm } from '../../core/types';
 import { form, FormField } from '@angular/forms/signals';
+
+const EMPTY_OFFER_FORM: OfferForm = {
+    platform: '',
+    skillsMust: '',
+    skillsPlus: '',
+    softSkills: '',
+    recruiters: '',
+    originalText: '',
+    salaryRange: {
+        min: null, max: null
+    },
+    role: '',
+    type: OFFER_TYPES.APPLICATION,
+    modality: OFFER_MODALITIES.HYBRID,
+    location: ''
+}
+
 @Component({
     selector: 'at-offer-form',
     imports: [FormField],
@@ -13,23 +30,10 @@ export class OfferFormComponent {
     private readonly mode: 'create' | 'update' = 'create';
     OFFER_TYPES = OFFER_TYPES;
     OFFER_MODALITIES = OFFER_MODALITIES;
+
     offersService = inject(OffersService);
 
-    offer = signal<OfferForm>({
-        platform: '',
-        skillsMust: '',
-        skillsPlus: '',
-        softSkills: '',
-        recruiters: '',
-        originalText: '',
-        salaryRange: {
-            min: null, max: null
-        },
-        role: '',
-        type: this.OFFER_TYPES.APPLICATION,
-        modality: this.OFFER_MODALITIES.HYBRID,
-        location: ''
-    })
+    offer = signal<OfferForm>(EMPTY_OFFER_FORM)
 
     offerForm = form(this.offer)
 
