5291deb8ac0c7c94b1950baba145ea57a27331f2
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 13:01:57 +0100

    DEV-0002 feat/ added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,36 @@
 import { Routes } from '@angular/router';
 import { BasePageComponent } from './base-page/base-page.component';
+import { ContactsPage } from './contacts-page/contacts-page';
+import { DashboardsPage } from './dashboards-page/dashboards-page';
+import { OffersPage } from './offers-page/offers-page';
+import { TipsPage } from './tips-page/tips-page';
+import { RoutinePage } from './routine-page/routine-page';
 
 export const routes: Routes = [
     {
         path: '',
         component: BasePageComponent,
         children: [
-            // {
-            //     path: '',
-            //     component: HomeComponent
-            // },
-            //             {
-            //     path: 'contact',
-            //     component: ContactComponent
-            // }
+            {
+                path: 'contacts',
+                component: ContactsPage
+            },
+            {
+                path: 'dashboard',
+                component: DashboardsPage
+            },
+            {
+                path: 'offers',
+                component: OffersPage
+            },
+            {
+                path: 'tips',
+                component: TipsPage
+            },
+            {
+                path: 'routine',
+                component: RoutinePage
+            }
         ]
     }
 ];
