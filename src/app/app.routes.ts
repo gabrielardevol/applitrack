63033e3cc68a95872547f021cd48557fffe17f7c
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-03-27 12:32:10 +0100

    DEV-0001 feat/ page layout + route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PageLayout } from './page-layout/page-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PageLayout,
+        children: [
+            // {
+            //     path: '',
+            //     component: HomeComponent
+            // },
+            //             {
+            //     path: 'contact',
+            //     component: ContactComponent
+            // }
+        ]
+    }
+];
