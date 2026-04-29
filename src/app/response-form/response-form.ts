5d2616666fc7bb6a89441bcb1ef52f0c44dc8cdc
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-04-29 13:33:16 +0200

    DEV-0010 feat/ template form adjustments

--- src/app/response-form/response-form.ts
+++ src/app/response-form/response-form.ts
@@ -1,14 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, signal, viewChild } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { form, FormField, required } from '@angular/forms/signals';
 import { OffersService } from '@app/offers/offers-service';
+import { ResponsesService } from '@app/responses/responses-service';
 import { EMPTY_RESPONSE_FORM } from '@app/shared/constants';
 import { LlmService } from '@app/shared/services/llm/llm-service';
 import { RESPONSE_TYPES, ResponseForm } from '@app/shared/types';
 
 @Component({
   selector: 'appli-response-form',
-  imports: [FormField, ReactiveFormsModule],
+  imports: [FormField,],
   templateUrl: './response-form.html',
   styleUrl: './response-form.scss',
 })
@@ -16,14 +17,12 @@ export class ResponseFormComponent {
   public RESPONSE_TYPES = RESPONSE_TYPES
   private response = signal<ResponseForm>(EMPTY_RESPONSE_FORM)
   private llmService = new LlmService<ResponseForm>;
+  //provide in root and type function?
   private offersService = new OffersService;
-
   offers = this.offersService.$listValue()
+  formTemplate = viewChild<HTMLFormElement>('#responseFormTemplate')
 
-  formDate = new FormControl()
-  formTime = new FormControl()
-  //¨provide in root and type function?
-
+  public responseService = inject(ResponsesService)
   public responseForm = form(this.response, (schemaPath) => {
     // required(schemaPath.role, { message: 'Required field' });
   })
@@ -51,5 +50,17 @@ export class ResponseFormComponent {
         this.responseForm().value.set(r)
       }
     )
+  }
+
+  public submitButtonClicked: boolean = false;
+
+  public submitForm() {
+    this.submitButtonClicked = true;
+    if (this.responseForm().valid()) {
+      this.responseService.create(this.responseForm().value());
+      this.responseForm().value.set(EMPTY_RESPONSE_FORM)
+      this.responseForm().reset()
+      this.submitButtonClicked = false;
+    }
   }
 }
