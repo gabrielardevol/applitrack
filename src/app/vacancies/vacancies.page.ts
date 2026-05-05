20d39123ff1b5dab5013c6f19310adb193a3c52c
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-05-05 17:12:12 +0200

    DEV-0035 feat/ sort vacancy list

--- src/app/vacancies/vacancies.page.ts
+++ src/app/vacancies/vacancies.page.ts
@@ -1,19 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, NgModule, signal } from '@angular/core';
 import { VacanciesService } from '../shared/services/vacancies/vacancy-service';
 import { VacancyDetail } from "./components/vacancy-detail/vacancy-detail";
-import { VacancyFormComponent } from './vacancy-form/vacancy-form.component';
-import { ResponseFormComponent } from '@app/responses/response-form/response-form';
 import { NgClass } from '@angular/common';
+import { VacancyListItem } from '@app/shared/types';
+import { form } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-vacancies-page',
+  providers: [NgModule],
   imports: [VacancyDetail, NgClass],
   templateUrl: './vacancies.page.html',
   styleUrl: './vacancies.page.scss',
 })
 export class VacanciesPage {
   private vacanciesService = inject(VacanciesService);
-  public vacancies = this.vacanciesService.$listValue;
+
+  private sortBy = signal<{ property: keyof VacancyListItem; order: 'ASC' | 'DESC' }>({ property: 'title', order: 'DESC' })
+
+  public sortForm = form(this.sortBy, (schemaPath) => {
+    // required(schemaPath.role, { message: 'Required field' });
+  })
+
+  public vacancies = computed(() => this.vacanciesService.$listValue().sort(
+    (a, b) => {
+      let comparison = (a as any)[this.sortBy().property].localeCompare((b as any)[this.sortBy().property]);
+      return this.sortBy().order == 'ASC' ? comparison : !comparison
+    }
+  ));
   view: 'table' | 'grid' = 'table';
 
   deleteVacancy(id: string) {
