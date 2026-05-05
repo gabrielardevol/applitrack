ecc47ee1bf28ce1da6f3c0ae098cd17c9886d0ef
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-05-05 18:28:46 +0200

    DEV-0035 feat/sort and order vacancies

--- src/app/vacancies/vacancies.page.ts
+++ src/app/vacancies/vacancies.page.ts
@@ -1,36 +1,48 @@
-import { Component, computed, inject, NgModule, signal } from '@angular/core';
+import { Component, computed, inject, NgModule, signal, WritableSignal } from '@angular/core';
 import { VacanciesService } from '../shared/services/vacancies/vacancy-service';
 import { VacancyDetail } from "./components/vacancy-detail/vacancy-detail";
 import { NgClass } from '@angular/common';
 import { VacancyListItem } from '@app/shared/types';
 import { form } from '@angular/forms/signals';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vacancies-page',
   providers: [NgModule],
-  imports: [VacancyDetail, NgClass],
+  imports: [VacancyDetail, NgClass, ReactiveFormsModule],
   templateUrl: './vacancies.page.html',
   styleUrl: './vacancies.page.scss',
 })
 export class VacanciesPage {
   private vacanciesService = inject(VacanciesService);
 
-  private sortBy = signal<{ property: keyof VacancyListItem; order: 'ASC' | 'DESC' }>({ property: 'title', order: 'DESC' })
+  private sortBy: WritableSignal<{ property: keyof VacancyListItem; order: 'ASC' | 'DESC' }> = signal({ property: 'createdAt', order: 'ASC' })
 
-  public sortForm = form(this.sortBy, (schemaPath) => {
-    // required(schemaPath.role, { message: 'Required field' });
-  })
+  sortPropertyFC = new FormControl('createdAt')
+  sortOrderFC = new FormControl('ASC')
 
-  public vacancies = computed(() => this.vacanciesService.$listValue().sort(
+  public vacancies = computed(() => [this.vacanciesService.$listValue().sort(
     (a, b) => {
       let comparison = (a as any)[this.sortBy().property].localeCompare((b as any)[this.sortBy().property]);
-      return this.sortBy().order == 'ASC' ? comparison : !comparison
+      return comparison;
     }
-  ));
+  )].map(items => {
+    return this.sortBy().order == 'ASC' ? items.reverse() : items
+  })[0]);
   view: 'table' | 'grid' = 'table';
 
   deleteVacancy(id: string) {
     this.vacanciesService.delete(id)
+  }
+
+  onSortChanges() {
+    this.sortBy.update(() => {
+      return {
+        property: this.sortPropertyFC.value as keyof VacancyListItem,
+        order: this.sortOrderFC.value as 'ASC' | 'DESC'
+      }
+    }
+    )
   }
 
 }
