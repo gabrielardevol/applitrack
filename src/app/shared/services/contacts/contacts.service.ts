0afac6822c368de5bb208368006190fca1a224e3
Gabriel Ardèvol <107632740+gabrielardevol@users.noreply.github.com>
2026-05-19 11:34:29 +0200

    DEV-0043 feat/ duplicate contact creation management

--- src/app/shared/services/contacts/contacts.service.ts
+++ src/app/shared/services/contacts/contacts.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BaseService } from '../base-service';
 import { Contact, ContactForm } from '@app/shared/types';
+import { NotificationService } from '../notifications/notification-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactsService extends BaseService<Contact, Contact, ContactForm> {
+
+  notificationsService = inject(NotificationService)
+
   constructor() {
     super('CONTACTS', '/contacts')
+  }
+
+  override create(contact: ContactForm) {
+
+    let filteredList = this.getByParams(
+      { name: contact.name, company: contact.company }
+    )
+    if (filteredList) {
+      this.notificationsService.createTemporaryNotification('Contact already exists')
+      return filteredList[0]
+    }
+    return super.create(contact)
   }
 }
